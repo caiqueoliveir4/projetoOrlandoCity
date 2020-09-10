ae8ef9600fc504ebb7406847363d83b7e43273c5
caiqueoliveir4 <caiquepark@hotmail.com>
2020-09-10 12:42:23 -0300

    novo projeto com correçao de erros no responsivo e no carousel

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# orlandocity
+projeto01
+>>>>>>> c5e5dbd15420f6fe34892c7c11bb34b18ce4be4e
---
+++ js/efeitos.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+	var owl = $(".owl-carousel");
+	owl.owlCarousel({
+		items: 5,
+		loop: true,
+		margin: 5,
+		autoplay: true,
+		autoplayTimeout: 2000,
+		autoplayHoverPause: true,
+		slideTransition: 'ease-in-out',
+		dots: false,
+		loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            480:{
+                items:3,
+				nav:true,
+				loop:true,
+            },
+            768:{
+                items:4,    
+                nav:true,
+                loop:true,
+            },
+            1200:{
+                items:6,
+                nav:true,
+                loop:true
+            }
+        }
+	});
+
+	$("#botao_anterior").on('click', function(){
+
+		owl.trigger('prev.owl.carousel');
+
+	});
+
+	$("#botao_proximo").on('click', function(){
+
+		owl.trigger('next.owl.carousel');
+
+	});
+	
+	
+	$("#logotipo").on("mouseover",function(){
+		
+		$("#banner h1").addClass("efeito");
+
+	}).on("mouseout", function(){
+
+		$("#banner h1").removeClass("efeito");
+
+	});
+		
+	$("#input-search").on("focus", function(){
+
+		$("li.search").addClass("ativo");
+
+	}).on("blur", function(){
+
+		$("li.search").removeClass("ativo");				
+
+	});
+	
+
+		
+	  
+	
+
+
+	 
+});
